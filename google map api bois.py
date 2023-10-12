620dfc0a5370a514c1053445274ea8e001256f65
suphadej anupan <suppadej3@gmail.com>
2023-10-12 12:55:20 +0700

    first commit

---
+++ google map api bois.py
@@ -0,0 +1,33 @@
+import googlemaps
+import json
+
+Api = googlemaps.Client(key='AIzaSyAS0YlCIA3Tdyf0QR1L2mctDMm5z4EkxTI')
+# location = '13.7349125,100.5304545'
+
+find_user_locate = Api.geolocate()
+
+find_user_locate_lat = find_user_locate["location"]["lat"]
+find_user_locate_lng = find_user_locate["location"]["lng"]
+
+if find_user_locate['accuracy'] > 3000:
+    location = input("")
+else:
+    location = str(find_user_locate_lat)+','+str(find_user_locate_lng)
+
+find_place = Api.places_nearby(location = location, radius = 4000, keyword = 'cafe')
+result = find_place['results']
+
+for i in range(len(result)):
+    name_place = result[i]['name']
+    place_locations = result[i]['vicinity']
+    geo_locate_lat = result[i]['geometry']['location']['lat']
+    geo_locate_lng = result[i]['geometry']['location']['lng']
+    destination_locate = str(result[i]['geometry']['location']['lat'])+','+str(result[i]['geometry']['location']['lng'])
+    distance = Api.distance_matrix(origins = location,destinations = destination_locate)
+    print(f"\nชื่อร้าน : {name_place}")
+    print(f"location : {place_locations}")
+    print(f"Geo_locate : {geo_locate_lat,geo_locate_lng}")
+    print(f"ระยะทางทั้งหมด : {distance['rows'][0]['elements'][0]['distance']['text']}")
+    print(f"ระยะเวลาที่ใช้ : {distance['rows'][0]['elements'][0]['duration']['text']}")
+
+print(f"จำนวนร้านที่พบ{i}")
