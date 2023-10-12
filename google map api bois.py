6a1c31dc112e3a9b90cba3f43f9ea028344752e1
hgokang <suppadej3@gmail.com>
2023-10-12 18:07:17 +0700

    first commint

--- google map api bois.py
+++ google map api bois.py
@@ -4,17 +4,17 @@ import googlemaps
 Api = googlemaps.Client(key='Test')
 # location = '13.7349125,100.5304545'
 
-find_user_locate = Api.geolocate()
+find_user_locate = Api.geolocate()                                                          #Api request
 
 find_user_locate_lat = find_user_locate["location"]["lat"]
 find_user_locate_lng = find_user_locate["location"]["lng"]
 
 if find_user_locate['accuracy'] > 3000:
-    location = input("")
+    location = input("กรอกที่อยู่")
 else:
     location = str(find_user_locate_lat)+','+str(find_user_locate_lng)
 
-find_place = Api.places_nearby(location = location, radius = 4000, keyword = 'cafe')
+find_place = Api.places_nearby(location = location, radius = 4000, keyword = 'cafe')        #Api request
 result = find_place['results']
 
 for i in range(len(result)):
@@ -23,7 +23,7 @@ for i in range(len(result)):
     geo_locate_lat = result[i]['geometry']['location']['lat']
     geo_locate_lng = result[i]['geometry']['location']['lng']
     destination_locate = str(geo_locate_lat)+','+str(geo_locate_lng)
-    distance = Api.distance_matrix(origins = location,destinations = destination_locate)
+    distance = Api.distance_matrix(origins = location,destinations = destination_locate)    #Api request ]Loop
     print(f"\nชื่อร้าน : {name_place}")
     print(f"location : {place_locations}")
     print(f"Geo_locate : {geo_locate_lat,geo_locate_lng}")
